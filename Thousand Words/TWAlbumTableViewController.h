e90542db411e2d9fa8afed3b64f6ae04703c7593
plehrer <plehrer@gmail.com>
2013-11-27 15:27:16 -0500

    Completed sections 8.8-10. Started Thousand Words app.

---
+++ Thousand Words/TWAlbumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TWAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Peter Lehrer on 11/27/13.
+//  Copyright (c) 2013 Peter Lehrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAlbumTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *albums;
+
+@end
