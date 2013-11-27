b4a40ca3bdb0b950f0d9ebe8eb6f3f467ca8b9e9
plehrer <plehrer@gmail.com>
2013-11-27 14:43:26 -0500

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Peter Lehrer on 11/27/13.
+//  Copyright (c) 2013 Peter Lehrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+	}
+}
